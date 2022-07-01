98768f32d235dbcd395bf143c33f1ef187699321
Jax Chu <294242589@qq.com>
2022-07-01 18:28:19 +0800

    feat: cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('./middlewares/cors');
 const indexRouter = require('./routes/index');
 const webRouter = require('./routes/web');
 const miniRouter = require('./routes/mini');
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors.allowAll);
 
 app.use('/', indexRouter);
 app.use('/web', webRouter);
