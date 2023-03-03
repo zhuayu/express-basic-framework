12debc05fbc8ef3ad769e8fd4eeccf3cc0250054
Jax <294242589@qq.com>
2023-03-03 18:01:27 +0800

    refactor: annotation router

--- routes/mini.js
+++ routes/mini.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const wechatController = require('./../controllers/wechat.js')
+// const wechatController = require('./../controllers/wechat.js')
 
-router.post('/wechat/oauth', wechatController.oAuthMini)
-router.post('/wechat/phone', wechatController.bindPhoneNumber)
+// router.post('/wechat/oauth', wechatController.oAuthMini)
+// router.post('/wechat/phone', wechatController.bindPhoneNumber)
 
 module.exports = router;
--- routes/web.js
+++ routes/web.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const wechatController = require('./../controllers/wechat.js')
-const carouselController = require('./../controllers/carousel.js')
-const feishuController = require('./../controllers/feishu.js')
+const carouselController = require('./../controllers/carousel.js');
+// const wechatController = require('./../controllers/wechat.js');
+// const feishuController = require('./../controllers/feishu.js');
 
 router.get('/carousel', carouselController.index);
-router.get('/wechat/oauth-url', wechatController.oAuthWebUrl);
-router.get('/wechat/oauth', wechatController.oAuthWeb);
+// router.get('/wechat/oauth-url', wechatController.oAuthWebUrl);
+// router.get('/wechat/oauth', wechatController.oAuthWeb);
 
-router.get('/feishu/token', feishuController.token);
-router.get('/feishu/app/:appToken', feishuController.appInfo);
-router.get('/feishu/app/:appToken/tables', feishuController.tables);
-router.get('/feishu/app/:appToken/tables/:tableId/fields', feishuController.fields);
-router.get('/feishu/app/:appToken/tables/:tableId/records', feishuController.records);
+// router.get('/feishu/token', feishuController.token);
+// router.get('/feishu/app/:appToken', feishuController.appInfo);
+// router.get('/feishu/app/:appToken/tables', feishuController.tables);
+// router.get('/feishu/app/:appToken/tables/:tableId/fields', feishuController.fields);
+// router.get('/feishu/app/:appToken/tables/:tableId/records', feishuController.records);
 
 module.exports = router;
