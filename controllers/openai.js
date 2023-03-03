44af392eca94bccfa42b0ab4ae42c59045446bdb
Jax <294242589@qq.com>
2023-03-03 19:36:44 +0800

    feat: openai

---
+++ controllers/openai.js
@@ -0,0 +1,22 @@
+const chatApi = require('./../threes/openai/chat_api.js');
+const schema = require('async-validator').default;
+
+const openAiController = {
+  completions: async function(req, res, next) {
+    const role = req.query.role
+    const content = req.query.content;
+    const validator = new schema({
+      role:  { type: 'string', required: true },
+      content:  { type: 'string', required: true },
+    })
+    try {
+      await validator.validate({ role, content })
+      const datas = await chatApi.completions(role, content);
+      res.json({error_code: 0, data: { datas } })
+    } catch (e) {
+      res.json({error_code: 1, message: e})
+    }
+  },
+}
+
+module.exports = openAiController;
---
+++ threes/openai/chat_api.js
@@ -0,0 +1,23 @@
+const axios = require('./axios.js');
+const OPENAI_KEY = process.env.OPENAI_KEY;
+
+const chatApi = {
+  completions: async function(role, content) {
+  	try{
+  		const API = `https://api.openai.com/v1/chat/completions`;
+  		const res = await axios.get(API, {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": role, "content": content}]
+        }, {
+        headers: {
+          'Authorization': `Bearer ${OPENAI_KEY}`
+        }
+      });
+  		return Promise.resolve(res);
+  	} catch (e) {
+  		return Promise.reject(e.response.data);
+  	}
+  },
+}
+
+module.exports = chatApi;
